d0a43622f04216b0917b9c2a6bb080f1eb4b7563
Chris Key <robertschris010@gmail.com>
2022-07-25 23:22:53 -0500

    updated questions

--- Develop/index.js
+++ Develop/index.js
@@ -11,24 +11,20 @@ const questions = [{
     name: "Title"
 }, {
     type: "input",
-    message: "What is the title of the project?",
+    message: "What is the description, installation instructions, usage information,contribution guidelines, and test instructions",
+    name: "Description, Installation, Usage, Contributing, and Tests"
+},{
+    type: "input",
+    message: "What licenses will be used?",
     name: "Title"
 },{
     type: "input",
-    message: "What is the title of the project?",
-    name: "Title"
+    message: "What is your Github username?",
+    name: "Username"
 },{
     type: "input",
-    message: "What is the title of the project?",
-    name: "Title"
-},{
-    type: "input",
-    message: "What is the title of the project?",
-    name: "Title"
-},{
-    type: "input",
-    message: "What is the title of the project?",
-    name: "Title"
+    message: "What is your email address?",
+    name: "Email"
 },
 ]
     
