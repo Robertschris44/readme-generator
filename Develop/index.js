e47b3132f6a3c91ae61fa0e62cde62fa974538d2
Chris Key <robertschris010@gmail.com>
2022-07-30 00:52:29 -0500

    updating writeTo file/added promise

--- Develop/index.js
+++ Develop/index.js
@@ -1,58 +1,92 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require("util");
+const util = require('util');
+
 
 const generatorMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [{
     type: "input",
     message: "What is the title of the project?",
-    name: "Title"
-}, {
+    name: "Title",
+    validate: nameInput =>{
+        if (nameInput) {
+            return true;
+        }else {
+            console.log('Enter name');
+            return false;
+        }
+    }
+}, 
+{
     type: "input",
-    message: "What is the description, installation instructions, usage information,contribution guidelines, and test instructions",
-    name: "Description, Installation, Usage, Contributing, and Tests"
-},{
-    type: "input",
-    message: "What licenses will be used?",
-    name: "Title"
-},{
+    message: "What is the description of your project?",
+    name: "Description",
+},
+{//change to list
+    type: "list",
+    message: "Choose a license.",
+    name: "License",
+    choices: ['Apache', 'IBM', 'MIT'],
+    validate: licenseInput = () =>{
+        if (licenseInput){
+            return true;
+        }else {
+            console.log('Please select options')
+            return false;
+        }
+    }
+},
+{
     type: "input",
     message: "What is your Github username?",
-    name: "Username"
-},{
+    name: "Username",
+},
+{
     type: "input",
     message: "What is your email address?",
-    name: "Email"
+    name: "Email",
 },
+{
+    type: "input",
+    message: "Table of Contents",
+    name: "Table of Contents",
+}
 ]
     
     
     
-
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err){
-        console.log(fileName)
-        console.log(data)
-        if (err) {
-            return console.log(err)
-        }else {
-            console.log("success")
-        }
-    })
-}
+const writeToFile = fileContent => {
+    return new Promise((resolve, reject) =>{
+        fs.writeFile('./generateREADME.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve((
+                true
+            ));
+        });
+
+    });
+
+};
+    
+
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then(function(data) {
-        writeToFile("README.md", generatorMarkdown(data));
-        console.log(data)
-    })
+        console.log(data);
+        fileContent = generatorMarkdown(data);
+       writeToFile(fileContent);
+    });
 }
 
 // Function call to initialize app
 init();
+
+module.exports = questions;
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,27 +4,76 @@ const inquirer = require('inquirer');
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "Apache") {
-      '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+      '![License](https://opensource.org/licenses/Apache-2.0)'
   } else if (license === "MIT") {
-      '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+      '![License: MIT](https://opensource.org/licenses/MIT)'
   } else if (license === "IBM") {
-      '![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+      '![License: IPL 1.0](https://opensource.org/licenses/IPL-1.0)'
   } else { '' }
 }
 return;
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = '';
+  if (license === 'Apache') {
+      licenseLink = '[![License](https://opensource.org/licenses/Apache-2.0)'
+  } else if (license === 'MIT') {
+      licenseLink = '[![License: MIT(https://opensource.org/licenses/MIT)'
+  } else if (license === 'IBM') {
+      licenseLink = '[![License: IPL 1.0](https://opensource.org/licenses/IPL-1.0)'
+  } else {
+      licenseLink = ''
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = '';
+  if (license === 'None') {
+      licenseSection = ''
+  } else {
+      licenseSection =
+          'License: ${license}'
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown(answer) {
+  return `# ${answer.title}
+## ${renderLicenseSection(answer.license)} ${renderLicenseBadge(answer.license)}
+### ${renderLicenseLink(answer.license)}
+7:29
+##Table of Contents
+-[License](#license)
+-[Installation](#installation)
+-[Usage](#usage)
+-[Features](#features)
+-[Credits](#contributors & credit)
+-[Tests](#tests)
+-[Email](#email)
+-[How to contribute](#how to contribute)
+7:30
+##Installation:
+### You must install the following for this app to function:
+- ${answer.installation}
+## Features:
+### ${answer.features}
+## Contributor:
+### ${answer.contribution}
+## Tests:
+### Run the following commands in your terminal to test this app:
+### ${answer.test}
+7:30
+## Questions:
+### If you have any questions, you may contact at either
+### Github: https://github.com/${answer.askMe}
+### or
+### Email: ${answer.email}
 `;
 }
 
